23bdf6e049fc1bcbac87630ce665ef8b1aa709b8
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-05-30 18:06:45 +0300

    implement ping function
    loop through request's abilities and all users abilities
    and find who can do the request

--- api/controllers/RequestController.js
+++ api/controllers/RequestController.js
@@ -6,6 +6,37 @@
  */
 
 module.exports = {
-	
-};
+	ping: function(req, res) {
+		// console.info(req.params);
 
+		Request.findOne(req.param("id")).exec(function(err, theRequest) {
+
+			// console.info(theRequest);
+
+			var userIds = [];
+
+			User.find().exec(function(err, allUsers) {
+				// console.info(allUsers);
+				// console.log(theRequest.abilities);
+				for (var i = 0; i < theRequest.abilities.length; i++) {
+					for (var j = 0; j < allUsers.length; j++) {
+						var curUser = allUsers[j];
+						for (var k = 0; k < curUser.abilities.length; k++) {
+							var curAbility = curUser.abilities[k];
+							if (theRequest.abilities[i] === curAbility) {
+								console.info('Found common abilities!', theRequest.abilities[i], curAbility);
+							}
+						}
+					}
+				}
+
+			});
+			// Get all users
+			// Loop users
+			//  and find the ones that match
+			//  loop ( abil as theRequest.abilities )
+			//    loop ( users )
+			//      if abil in users.abilities userIds.push(user.id)
+    });
+	}
+};
